e182366c135eae9b1a1fac84bb7562627aec498d
Nattawin <iambeboy@gmail.com>
2018-01-30 16:03:12 +0700

    initial commit

--- docker/server.js
+++ docker/server.js
@@ -4,7 +4,7 @@ var os = require('os');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Welcome to Kubernetes\nServing from Host: '+ os.hostname() );
+  response.end('Welcome to myNode\nServing from Host: '+ os.hostname() );
 };
 
 var www = http.createServer(handleRequest);
