e9aa43013c1eb81c672125de5b421500996d92d8
Intachompoo <005961@g-able.com>
2018-04-04 11:26:15 +0700

    initial commit

--- docker/server.js
+++ docker/server.js
@@ -4,7 +4,7 @@ var os = require('os');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello Master\nServing from Host: '+ os.hostname() );
+  response.end('Hello Master_2.0\nServing from Host: '+ os.hostname() );
 };
 
 var www = http.createServer(handleRequest);
